b8ec2bd636cf8b4e24ccea2390b57a3e89fbe69a
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-09-25 22:27:33 -0500

    Update frontend with some Jest test

--- front-end/frontend-react-app/src/App.test.tsx
+++ front-end/frontend-react-app/src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LoginPanel from "./LogIn";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("UserID input", async () => {
+  render(
+    <BrowserRouter>
+      <LoginPanel />
+    </BrowserRouter>
+  );
+  const userIDInputField = screen.getByTestId("userID");
+  await userEvent.type(userIDInputField, "danielnguyen");
+  expect(userIDInputField).toHaveValue("danielnguyen");
+});
+
+test("Password input", async () => {
+  render(
+    <BrowserRouter>
+      <LoginPanel />
+    </BrowserRouter>
+  );
+  const passwordInputField = screen.getByTestId("password");
+  await userEvent.type(passwordInputField, "12345");
+  expect(passwordInputField).toHaveValue("12345");
+});
+
+test("Login button click", async () => {
+  render(
+    <BrowserRouter>
+      <LoginPanel />
+    </BrowserRouter>
+  );
+  const userIDInputField = screen.getByTestId("userID");
+  const passwordInputField = screen.getByTestId("password");
+  const loginButton = screen.getByTestId("loginButton");
+  await userEvent.type(userIDInputField, "tannguyen");
+  await userEvent.type(passwordInputField, "14we4");
+  await userEvent.click(loginButton);
 });
