163bad8bfcbe49723105e1e6b33ed1595142dd76
Haylen Sewell <hnsewell@uiowa.edu>
2023-10-04 13:39:26 -0500

    basic components of home / welcome page

--- front-end/frontend-react-app/src/Home.tsx
+++ front-end/frontend-react-app/src/Home.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Center, Square, Circle } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react'
 
 export default function HomePage() {
   useEffect(() => {
@@ -7,8 +11,22 @@ export default function HomePage() {
   }, []);
 
   return (
-    <Flex>
+    <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
+      <Center>
+      <VStack spacing={4} align='stretch'>
       <Heading>This is Home page</Heading>
-    </Flex>
+      <HStack spacing='24px' >
+      <Spacer />
+        <Link href='/login'>
+        <Button colorScheme='teal'>Log In</Button>
+        </Link>
+        <Link href='/signup'>
+        <Button colorScheme='teal'>Sign Up</Button>
+        </Link>
+      <Spacer />
+      </HStack>
+      </VStack>
+      </Center>
+      </Flex>
   );
 }
