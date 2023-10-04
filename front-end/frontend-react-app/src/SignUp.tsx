1611afed573cbffec25eddb22be932d80fbbd239
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-10-04 00:36:41 -0500

    Add more input fields and security question

--- front-end/frontend-react-app/src/SignUp.tsx
+++ front-end/frontend-react-app/src/SignUp.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Flex, Heading, Input, Button, Wrap, Select } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  Wrap,
+  Select,
+  Text,
+  Stack,
+  HStack,
+} from "@chakra-ui/react";
 
 export default function SignUpPanel() {
+  let counter = 0;
   useEffect(() => {
     document.title = "Sign Up - Voting System";
   }, []);
 
   const initialValues = {
-    ID: "",
     firstName: "",
     middleName: "",
     lastName: "",
@@ -18,14 +28,18 @@ export default function SignUpPanel() {
     state: "",
     zip: "",
     email: "",
+    password: "",
     phone: "",
     driverID: "",
+    questionIndex: "",
+    securityAnswer: "",
   };
 
   //Input listeners
   const [inputValue, setInputValue] = React.useState(initialValues);
   const handleInput = (e: { target: { name: any; value: any } }) => {
     const { name, value } = e.target;
+    counter++;
     setInputValue({
       ...inputValue,
       [name]: value,
@@ -49,7 +63,13 @@ export default function SignUpPanel() {
 
   //Signup button listener
   const navigate = useNavigate();
+  const [decision, setDecision] = React.useState(false);
   const handleSignup = async () => {
+    //Check if all the field is filled
+    if (counter < 11) {
+      setDecision(true);
+    }
+
     //Stringify the value to be in JSON file for backend retrieval. Fetch should have the backend's url.
     await fetch("http://localhost:5000/signup", {
       method: "POST",
@@ -67,13 +87,44 @@ export default function SignUpPanel() {
         state: inputValue["state"],
         zip: inputValue["zip"],
         email: inputValue["email"],
+        password: inputValue["password"],
         phone: inputValue["phone"],
         driverID: inputValue["driverID"],
-        ID: inputValue["ID"],
+        questionIndex: inputValue["questionIndex"],
+        securityAnswer: inputValue["securityAnswer"],
       }),
     }).catch((error) => console.log(error));
-
     navigate("/login");
+  };
+
+  //Password retype listener
+  const [inputRetype, setRetypePass] = React.useState("");
+  const handleRetypePassword = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setRetypePass(e.target.value);
+  };
+
+  //Show invalid retype password
+  const InvalidPassword = () => {
+    return (
+      <>
+        <Text data-testid="invalidInput" color="red" mb={3}>
+          *Your password does not match up*
+        </Text>
+      </>
+    );
+  };
+
+  //Show not all field has been filled
+  const UnfilledFields = () => {
+    return (
+      <>
+        <Text data-testid="invalidInput" color="red" mb={3}>
+          *Please fill out all necessary field*
+        </Text>
+      </>
+    );
   };
 
   //Container for state options
@@ -82,7 +133,7 @@ export default function SignUpPanel() {
       <option selected hidden disabled value="">
         State
       </option>
-      <option value="AL">AL</option>
+      <option value={inputValue["state"]}>AL</option>
       <option value={inputValue["state"]}>AK</option>
       <option value={inputValue["state"]}>AZ</option>
       <option value={inputValue["state"]}>AR</option>
@@ -135,8 +186,32 @@ export default function SignUpPanel() {
     </>
   );
 
+  //Container for security questions
+  const questionOption = (
+    <>
+      <option value={inputValue["questionIndex"]}>
+        What is your mother's hometown?
+      </option>
+      <option value={inputValue["questionIndex"]}>
+        What is your chilhood's name?
+      </option>
+      <option value={inputValue["questionIndex"]}>
+        What is your elementary school name?
+      </option>
+      <option value={inputValue["questionIndex"]}>
+        How many pets do you currently have?
+      </option>
+      <option value={inputValue["questionIndex"]}>
+        What is your grandmotherr's first name?
+      </option>
+      <option value={inputValue["questionIndex"]}>
+        What is your favorite country to visit?
+      </option>
+    </>
+  );
+
   return (
-    <Flex height="70vh" alignItems="left" justifyContent="center">
+    <Flex height="auto" alignItems="left" justifyContent="center">
       <Flex
         width="1000px"
         alignItems="center"
@@ -147,6 +222,13 @@ export default function SignUpPanel() {
       >
         <Heading mb={6}>Sign up for Voting</Heading>
         <Wrap>
+          {decision && <UnfilledFields></UnfilledFields>}
+          <HStack>
+            <Text fontSize="sm">Name and Address: </Text>
+            <Text fontSize="xs" color="red">
+              *
+            </Text>
+          </HStack>
           <Input
             name="firstName"
             onChange={handleInput}
@@ -194,7 +276,12 @@ export default function SignUpPanel() {
             mb={3}
             background="gray.200"
           ></Input>
-          <Select name="stateSelect" width={24} borderWidth={2}>
+          <Select
+            name="state"
+            width={24}
+            borderWidth={2}
+            onChange={handleInput}
+          >
             {stateOption}
           </Select>
           <Input
@@ -207,28 +294,95 @@ export default function SignUpPanel() {
             mb={3}
             background="gray.200"
           ></Input>
-        </Wrap>
-        <Wrap justify="start" mt={2}>
-          <Input
-            name="email"
-            width={500}
-            onChange={handleInput}
-            value={inputValue["email"]}
-            placeholder="Email"
-            variant="filled"
-            mb={3}
-            background="gray.200"
-          ></Input>
-          <Input
-            name="phone"
-            width={412}
-            onChange={handleInput}
-            value={inputValue["phone"]}
-            placeholder="Phone Number"
-            variant="filled"
-            mb={3}
-            background="gray.200"
-          ></Input>
+          <Stack justify="left">
+            <HStack>
+              <Text fontSize="sm">Username and Password:</Text>
+              <Text fontSize="xs" color="red">
+                *
+              </Text>
+            </HStack>
+            <Input
+              name="email"
+              width={500}
+              onChange={handleInput}
+              value={inputValue["email"]}
+              placeholder="Email"
+              variant="filled"
+              mb={3}
+              background="gray.200"
+            ></Input>
+            <Input
+              name="password"
+              width={500}
+              onChange={handleInput}
+              value={inputValue["password"]}
+              placeholder="Password"
+              variant="filled"
+              mb={3}
+              background="gray.200"
+              type="password"
+            ></Input>
+            <HStack>
+              <Input
+                name="passwordRetype"
+                width={500}
+                onChange={handleRetypePassword}
+                placeholder="Retype your password"
+                variant="filled"
+                mb={3}
+                background="gray.200"
+                type="password"
+              ></Input>
+              {inputRetype !== inputValue["password"] && (
+                <InvalidPassword></InvalidPassword>
+              )}
+            </HStack>
+            <HStack>
+              <Text fontSize="sm">Additional verifications:</Text>
+              <Text fontSize="xs" color="red">
+                *
+              </Text>
+            </HStack>
+            <Input
+              name="phone"
+              width={412}
+              onChange={handleInput}
+              value={inputValue["phone"]}
+              placeholder="Phone Number"
+              variant="filled"
+              mb={3}
+              background="gray.200"
+            ></Input>
+            <Input
+              name="driverID"
+              width={412}
+              onChange={handleInput}
+              value={inputValue["driverID"]}
+              placeholder="Driver License ID"
+              variant="filled"
+              mb={6}
+              background="gray.200"
+            ></Input>
+            <HStack>
+              <Text fontSize="sm">Security Questions:</Text>
+              <Text fontSize="xs" color="red">
+                *
+              </Text>
+            </HStack>
+            <Select name="questionIndex" borderWidth={2} onChange={handleInput}>
+              {questionOption}
+            </Select>
+            <Input
+              name="securityAnswer"
+              width={500}
+              onChange={handleInput}
+              value={inputValue["securityAnswer"]}
+              placeholder="Answer"
+              variant="filled"
+              mb={6}
+              background="gray.200"
+            ></Input>
+          </Stack>
         </Wrap>
         <Wrap spacing="20px">
           <Button colorScheme="teal" onClick={handleSignup}>
@@ -238,6 +392,9 @@ export default function SignUpPanel() {
             Cancel
           </Button>
         </Wrap>
+        <Text fontSize="xs" mt={10}>
+          Voting System
+        </Text>
       </Flex>
     </Flex>
   );
