0fe98bf94bca399778887291c686720f350fa7de
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-10-08 23:05:21 -0500

    Initialize Forgot password page.

--- front-end/frontend-react-app/src/index.tsx
+++ front-end/frontend-react-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "../reportWebVitals";
+import App from "./Routings/App";
+import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
