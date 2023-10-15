76d92e88a4501d934a7282f3568c98ad66127a32
Haylen Sewell <hnsewell@uiowa.edu>
2023-10-15 10:55:34 -0500

    home page

--- front-end/frontend-react-app/src/Home.tsx
+++ front-end/frontend-react-app/src/Home.tsx
@@ -4,29 +4,68 @@ import { Center, Square, Circle } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Stack, HStack, VStack } from '@chakra-ui/react'
 import { Link } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import NavBar from "./NavBar"
+import { Container } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 export default function HomePage() {
+
   useEffect(() => {
     document.title = "Home - Voting System";
   }, []);
-
-  return (
-    <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
-      <Center>
-      <VStack spacing={4} align='stretch'>
-      <Heading>This is Home page</Heading>
-      <HStack spacing='24px' >
-      <Spacer />
-        <Link href='/login'>
-        <Button colorScheme='teal'>Log In</Button>
-        </Link>
-        <Link href='/signup'>
-        <Button colorScheme='teal'>Sign Up</Button>
-        </Link>
-      <Spacer />
-      </HStack>
+  
+  return(
+    <>
+  <NavBar />
+  <VStack>
+    <Spacer />
+    <Spacer />
+    <Spacer />
+    <Box padding='4' bg='teal.400' color='white' w="85%" border='2px' borderColor='teal'>
+    RIGHT NOW: Candidate A vs Candidate B in Random Location's Presidental Race
+    </Box>
+    <HStack w="85%">
+      <Container w="50px">
+      </Container>
+      <VStack>
+        <Heading>Your Vote Matters!</Heading>
+        <Container maxW="600">
+          <Image boxSize='350px' w="500" objectFit='cover' src='https://s7d2.scene7.com/is/image/TWCNews/istock-generic-election-buttons-votingjpg3840x2160jpg' alt='Voting Icon' />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+          cupidatat non proident, sunt in
+        </Container>
       </VStack>
-      </Center>
-      </Flex>
-  );
+      <VStack>
+        <Container maxW='2xl' centerContent>
+          <Box padding='4' bg='gray.100' color='black' maxW='md' border='2px' borderColor='teal'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+          nisi ut aliquip ex ea commodo consequat. 
+          </Box>
+        </Container>
+        <Container maxW='2xl' centerContent>
+          <Box padding='4' bg='gray.100' color='black' maxW='md' border='2px' borderColor='teal'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequat. 
+          </Box>
+        </Container>
+        <Container maxW='2xl' centerContent>
+          <Box padding='4' bg='gray.100' color='black' maxW='md' border='2px' borderColor='teal'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequat. 
+          </Box>
+        </Container>
+      </VStack>
+      <Container w="50px">
+      </Container>
+    </HStack>
+  </VStack>
+</>
+  )
 }
---
+++ front-end/frontend-react-app/src/NavBar.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Center, Square, Circle } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+
+export default function NavBar() {
+    var isLoggedIn = false;
+
+    if (isLoggedIn){
+        return (
+          <Flex minH={'60px'}  py={{ base: 2 }} px={{ base: 4 }} borderBottom={3} borderStyle={'solid'} 
+          borderColor={'teal'} background="gray.100" p={4} align={'center'}>
+            <Flex flex={{base:1}} justify={{base: 'center', md: 'start'}}>
+              <Image boxSize='50px' objectFit='cover' src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3415254/election-day-clipart-md.png' alt='Voting Icon' />
+              <HStack spacing='24px' >
+                <Spacer />
+                <Heading>My Vote</Heading>
+              </HStack>
+            </Flex>
+    
+            <Flex flex={{base:1}} justify={{base: 'center', md: 'end'}}>
+              <HStack spacing='24px'>
+                <Button colorScheme='teal'>User Profile</Button>
+                <Link href='/login'>
+                <Button colorScheme='teal'>
+                Log Out
+                </Button>
+                </Link>
+              </HStack>
+            </Flex>
+          </Flex>
+        );
+      }
+    
+      else{
+        return (
+          <Flex minH={'60px'}  py={{ base: 2 }} px={{ base: 4 }} borderBottom={3} borderStyle={'solid'} 
+          borderColor={'teal'} background="gray.100" p={4} align={'center'}>
+            <Flex flex={{base:1}} justify={{base: 'center', md: 'start'}}>
+              <Image boxSize='50px' objectFit='cover' src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3415254/election-day-clipart-md.png' alt='Voting Icon' />
+              <HStack spacing='24px' >
+                <Spacer />
+                <Link href='/home'>
+                <Heading>My Vote</Heading>
+                </Link>
+              </HStack>
+            </Flex>
+    
+            <Flex flex={{base:1}} justify={{base: 'center', md: 'end'}}>
+              <HStack spacing='24px'>
+                <Link href='/login'>
+                  <Button colorScheme='teal'>Log in</Button>
+                </Link>
+                <Link href='/signup'>
+                  <Button colorScheme='teal'>Sign up</Button>
+                </Link>
+              </HStack>
+            </Flex>
+         </Flex>
+        );
+      }
+}
+
+export{}
