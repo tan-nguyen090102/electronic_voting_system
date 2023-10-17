f416c211f85483211563d7add1c9f8545ba411a5
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-10-16 21:31:32 -0500

    Clean up some incoming files.

--- frontend/src/Features/NavBar.tsx
+++ frontend/src/Features/NavBar.tsx
@@ -1,63 +1,68 @@
-import { Flex, Heading, Spacer } from "@chakra-ui/react";
-import { Button } from '@chakra-ui/react'
-import { HStack } from '@chakra-ui/react'
-import { Link } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
+import { Flex, Heading, Spacer, Button, HStack, Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-export default function NavBar() {
-    var isLoggedIn = false;
-
-    if (isLoggedIn){
-        return (
-          <Flex minH={'60px'}  py={{ base: 2 }} px={{ base: 4 }} borderBottom={3} borderStyle={'solid'} 
-          borderColor={'teal'} background="gray.100" p={4} align={'center'}>
-            <Flex flex={{base:1}} justify={{base: 'center', md: 'start'}}>
-              <Image boxSize='50px' objectFit='cover' src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3415254/election-day-clipart-md.png' alt='Voting Icon' />
-              <HStack spacing='24px' >
-                <Spacer />
-                <Heading>My Vote</Heading>
-              </HStack>
-            </Flex>
-    
-            <Flex flex={{base:1}} justify={{base: 'center', md: 'end'}}>
-              <HStack spacing='24px'>
-                <Button colorScheme='teal'>User Profile</Button>
-                <Link href='/login'>
-                <Button colorScheme='teal'>
-                Log Out
-                </Button>
-                </Link>
-              </HStack>
-            </Flex>
-          </Flex>
-        );
-      }
-    
-      else{
-        return (
-          <Flex minH={'60px'}  py={{ base: 2 }} px={{ base: 4 }} borderBottom={3} borderStyle={'solid'} 
-          borderColor={'teal'} background="gray.100" p={4} align={'center'}>
-            <Flex flex={{base:1}} justify={{base: 'center', md: 'start'}}>
-              <Image boxSize='50px' objectFit='cover' src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3415254/election-day-clipart-md.png' alt='Voting Icon' />
-              <HStack spacing='24px' >
-                <Spacer />
-                <Heading>My Vote</Heading>
-              </HStack>
-            </Flex>
-    
-            <Flex flex={{base:1}} justify={{base: 'center', md: 'end'}}>
-              <HStack spacing='24px'>
-                <Link href='/login'>
-                  <Button colorScheme='teal'>Log in</Button>
-                </Link>
-                <Link href='/signup'>
-                  <Button colorScheme='teal'>Sign up</Button>
-                </Link>
-              </HStack>
-            </Flex>
-         </Flex>
-        );
-      }
+interface Props {
+  isLoggedIn: boolean;
 }
 
-export{}
+export default function NavBar(props: Props) {
+  const isLoggedIn = props.isLoggedIn;
+
+  //Login/out button listener
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  //Sign up button listener
+  const handleSignup = () => {
+    navigate("/signup");
+  };
+
+  //User profile listener
+  const handleUserProfile = () => {
+    navigate("/user_profile");
+  };
+
+  //DOM
+  return (
+    <Flex
+      minH={"60px"}
+      py={{ base: 2 }}
+      px={{ base: 4 }}
+      borderBottom={3}
+      borderStyle={"solid"}
+      borderColor={"teal"}
+      background="gray.100"
+      p={4}
+      align={"center"}
+    >
+      <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+        <Image
+          boxSize="50px"
+          objectFit="cover"
+          src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3415254/election-day-clipart-md.png"
+          alt="Voting Icon"
+        />
+        <HStack spacing="24px">
+          <Spacer />
+          <Heading>My Vote</Heading>
+        </HStack>
+      </Flex>
+
+      <Flex flex={{ base: 1 }} justify={{ base: "center", md: "end" }}>
+        <HStack spacing="24px">
+          <Button colorScheme="teal" onClick={handleLogin}>
+            {isLoggedIn ? "Log out" : "Log in"}
+          </Button>
+          <Button
+            colorScheme="teal"
+            onClick={isLoggedIn ? handleUserProfile : handleSignup}
+          >
+            {isLoggedIn ? "User Profile" : "Sign up"}
+          </Button>
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+}
