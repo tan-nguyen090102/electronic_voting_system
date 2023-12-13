3c48ecd5b5934577ec9ffb8b50508f560f272523
Haylen Sewell <hnsewell@uiowa.edu>
2023-12-13 15:08:43 -0600

    twostep verification

--- frontend/src/Features/TwoStepVerification.tsx
+++ frontend/src/Features/TwoStepVerification.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   Button,
   Flex,
@@ -7,11 +7,19 @@ import {
   Input,
   Center,
   Text,
-  Stack
+  Stack,
+  Wrap
 } from "@chakra-ui/react";
+import NavBar from "./NavBar";
 
 
 export default function TwoStepPanel(){
+
+  const { state } = useLocation();
+  const { user, code, requestedPage, linkPath } = state || { user: "", code: "", requestedPage: "", linkPath: "" };
+
+  const [isErrorPopUp, setErrorPopUp] = React.useState(false);
+
     useEffect(() => {
         document.title = "Verification - Voting System";
       }, []);
@@ -31,16 +39,40 @@ export default function TwoStepPanel(){
   };
 
   const navigate = useNavigate();
+
+  const handleCheck = () =>{
+    if (inputValue.code === code){
+      setErrorPopUp(false)
+      if (requestedPage === "forgot"){
+        navigate("/change_password", { state: { user: user }})
+      }
+      else if (requestedPage === "login"){
+        navigate(linkPath, { state: { user: user, isLoggedIn: "true" }})
+      }
+    }
+    else {
+      setErrorPopUp(true)
+    }
+  }
+
   const handleCancel = () => {
     navigate("/login");
   };
 
 
       return (
+        <div>
+        <NavBar
+        title={"Help"}
+        isLoggedIn="false"
+        isBlank="true"
+        userName={""}
+        role="voter"
+      ></NavBar>
         <Flex height="100vh" alignItems="center" justifyContent="center">
           <Flex direction="column" background="gray.100" p={12} rounded={6}>
             <Heading><Center>Enter the code</Center></Heading>
-            <Center><Text mb={6}>We've sent an x-digit code to useremail@email.com.</Text>
+            <Center><Text mb={6}>We've sent a 6-digit code to {user}.</Text>
             </Center>
             <Input
             name="code"
@@ -52,9 +84,15 @@ export default function TwoStepPanel(){
             mb={3}
             background="gray.200"
           ></Input>
+            {isErrorPopUp && (
+            <Text data-testid="invalidInput" color="red" mb={3}>
+                *Invalid Code*
+            </Text>
+            )}
           <Stack>
           <Button
-           colorScheme="teal">
+           colorScheme="teal"
+           onClick={handleCheck}>
             Submit
             </Button>
             <Button
@@ -64,7 +102,13 @@ export default function TwoStepPanel(){
             Cancel
             </Button>
             </Stack>
+            <Wrap justify="center">
+            <Text fontSize="xs" mt={6}>
+              Voting System
+            </Text>
+          </Wrap>
           </Flex>
         </Flex>
+        </div>
       )
 }
